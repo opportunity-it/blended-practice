df4f776bffed8187c3a83cb8dc58fb2d31f202ca
YuraIvashkiv <113311064+YuraIvashkiv@users.noreply.github.com>
2023-06-18 12:56:41 +0000

    Deploying to gh-pages from @ YuraIvashkiv/blended-practice@cefb1beb0678d380af05b129aebfd6fb59187259 🚀

--- index.9197b5f3.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("#productTable"),e=document.querySelector("#productDetails");t.addEventListener("click",(function(t){if("TD"!==t.target.nodeName)return;var n=t.target.parentNode,r=n.firstElementChild.textContent,c=n.lastElementChild.textContent;e.innerHTML="Ви вибрали ".concat(r," за ").concat(c)}))}();
-//# sourceMappingURL=index.9197b5f3.js.map
