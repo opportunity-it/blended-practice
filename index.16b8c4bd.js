18bcf6055ea409a8531491a475f860c793045978
YuraIvashkiv <113311064+YuraIvashkiv@users.noreply.github.com>
2023-06-18 14:12:48 +0000

    Deploying to gh-pages from @ YuraIvashkiv/blended-practice@95083921d8926e342bc071c410facc00abc8c36f 🚀

--- index.16b8c4bd.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector(".todo"),e=(document.querySelector(".todo__items"),{});t.addEventListener("input",(function(t){var o=t.target,n=o.name,r=o.value;e[n]=r,localStorage.setItem("todo",JSON.stringify(e))}))}();
-//# sourceMappingURL=index.16b8c4bd.js.map
--- index.b70008f5.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector(".todo"),e=(document.querySelector(".todo__items"),{});t.addEventListener("input",(function(t){const{name:o,value:n}=t.target;e[o]=n,localStorage.setItem("todo",JSON.stringify(e))}));
-//# sourceMappingURL=index.b70008f5.js.map
